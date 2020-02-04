0bd9c0aeed069a675ebf0e6190a8eb03c3ef4ed9
Qadzif Kamil <qadzifi@gmail.com>
2020-02-04 13:16:57 +0700

    Create base page

--- boxinger/settings.py
+++ boxinger/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = 'boxinger.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
+#		printf "%s%s\n" "$year_num" "$current_term"
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATUCFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
